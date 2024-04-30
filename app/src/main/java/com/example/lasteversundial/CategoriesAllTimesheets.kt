03c6d33fe1aa6f48e19389bb9b5de5f7ed909f25
Dillon-Duncan <mrduncan.business@gmail.com>
2024-04-30 18:41:21 +0200

    Working Commit

---
+++ app/src/main/java/com/example/lasteversundial/CategoriesAllTimesheets.kt
@@ -0,0 +1,11 @@
+package com.example.lasteversundial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoriesAllTimesheets : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categories_all_timesheets)
+    }
+}
---
+++ app/src/main/java/com/example/lasteversundial/SearchTimesheetPeriod.kt
@@ -0,0 +1,11 @@
+package com.example.lasteversundial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchTimesheetPeriod : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_timesheet_period)
+    }
+}
